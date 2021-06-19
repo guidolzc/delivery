b3c016e8c579242352132285404799efb7e8dca2
guidolzc <pcigfernandez@gmail.com>
2021-06-19 15:57:28 -0700

    deliver

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+let btnMenu = document.getElementById('btnmenu');
+let menu = document.getElementById('Menu');
+btnMenu.addEventListener('click', function(){
+    'use strict';
+    menu.classList.toggle('mostrar');
+});   
+
+
---
+++ js/opacar.js
@@ -0,0 +1,6 @@
+let btnm = document.getElementById('btnmenu');
+let mainh = document.getElementById('banner__content');
+btnm.addEventListener('click', function(){
+  'use strict';
+  mainh.classList.toggle('opacar');
+});
